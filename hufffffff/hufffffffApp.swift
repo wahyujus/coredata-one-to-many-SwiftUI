032461df014b334c18f4d6ba5cfc9f94df55b872
wahyujus <wahyukharismaz@gmail.com>
2020-10-16 15:40:19 +0700

    Initial Commit

---
+++ hufffffff/hufffffffApp.swift
@@ -0,0 +1,17 @@
+//
+//  hufffffffApp.swift
+//  hufffffff
+//
+//  Created by wahyujus on 16/10/20.
+//
+
+import SwiftUI
+
+@main
+struct hufffffffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
